c7ebdc1445e1eb7eecdf2a7b3fbebaaab9856753
mjwrapp <joshi789mahi2@gmail.com>
2026-03-16 13:16:33 +0530

    fixing backend

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -1,23 +1,26 @@
-import jwt from"jsonwebtoken"
+import jwt from "jsonwebtoken"
 
-const isAuth = async(req,res,next)=>{
+const isAuth = async (req, res, next) => {
   try {
-    let {token} = req.cookies
 
-    if(!token){
-      return res.status(400,json({message:"user does not have a token"}))
-    }
-    const verifyToken = jwt.verify(token,process.env.JWT_SCERET)
+    const { token } = req.cookies
 
-    if(!verifyToken){
-     return res.status(400,json({message:"user does not have a  valid token"}))
+    if (!token) {
+      return res.status(400).json({ message: "user does not have a token" })
     }
+
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET)
+
+    if (!verifyToken) {
+      return res.status(400).json({ message: "user does not have a valid token" })
+    }
+
     req.userId = verifyToken.userId
 
     next()
 
   } catch (error) {
-      return res.status(500).json({message:` isauth error ${error}`})
+    return res.status(500).json({ message: `isauth error ${error}` })
   }
 }
 
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,12 @@
- import express from "express"
-import {googleAuth,logOut} from "../controllers/auth.controller.js"
+import express from "express"
+import { googleAuth, logOut } from "../controllers/auth.controller.js"
+import { getCurrentUser } from "../controllers/user.controller.js"
+import isAuth from "../middlewares/isAuth.js"
 
- const authRouter = express.Router()
+const authRouter = express.Router()
 
- authRouter.post("/google",googleAuth)
- authRouter.get("/logout",logOut)
+authRouter.post("/google", googleAuth)
+authRouter.get("/logout", logOut)
+authRouter.get("/currentuser", isAuth, getCurrentUser)
 
- export default authRouter
+export default authRouter
