779d53bd4d5e7e602f38244aebcc1ce3b9a5e059
mjwrapp <joshi789mahi2@gmail.com>
2026-03-18 02:08:31 +0530

    frontend homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import Auth from "./pages/Auth"
+import { useEffect } from "react"
+import axios from "axios"
+import { useDispatch } from "react-redux"
+import { setUserData } from "./redux/userslice"
 
 export const ServerURL = "http://localhost:8000"
 
 function App() {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const result = await axios.get(ServerURL+"/api/auth/currentuser",{withCredentials:true} )
+        dispatch(setUserData(result.data))
+      } catch (error) {
+        dispatch(setUserData(null))
+      }
+    }
+    getUser()
+  }, [dispatch])
   return (
     <Routes>
       <Route path='/' element={<Home />} />
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -7,9 +7,14 @@ import { signInWithPopup } from 'firebase/auth';
 import { provider } from '../utils/firebase';
 import { auth } from '../utils/firebase'; 
 import axios from "axios";
+
+import { useDispatch } from "react-redux"
+import { setUserData } from "../redux/userslice"
+
 const ServerURL = "http://localhost:8000";
 
-function Auth() {
+function Auth( {isModel =false} ) {
+  const dispatch = useDispatch()
   const handelGoogleAuth = async() => {
     try {
       const response = await signInWithPopup(auth,provider)
@@ -17,20 +22,19 @@ function Auth() {
       let name = User.displayName
       let email = User.email
       const result = await axios.post(ServerURL+"/api/auth/google",{name,email},{withCredentials:true })
-      console.log(result.data)
+       dispatch(setUserData(result.data))
     } catch (error) {
-      console.log(error)
+      dispatch(setUserData(null))
     }
   }
   return (
-    <div className='w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20'>
+   <div className={`w-full ${isModel ? "py-4" : "min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20"}`}>
       
       <motion.div
         initial={{ opacity: 0, y: -40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.05 }}
-        className='w-full max-w-md p-8 rounded-3xl bg-white shadow-2xl border border-gray-200'
-      >
+       className='w-full  ${isModel ? "max-w-md p-8 rounded-3xl" : "max-w-lg p-12 rounded-[32px]"} bg-white shadow-2xl border border-gray-200'>
         
         <div className='flex items-center justify-center gap-3 mb-6'>
           <div className='bg-black text-white p-2 rounded-lg'>
@@ -53,7 +57,7 @@ function Auth() {
         </p>
         <motion.button 
         onClick={handelGoogleAuth}
-         whileHover={{opacity:0.5,scale:1.03}}
+         whileHover={{opacity:0.9,scale:1.03}}
          whileTap={{opacity:1,scale:0.98}}
         className='w-full flex items-center justify-center gap-3 py-3 bg-black text-white rounded-full shadow-md'>
         <FcGoogle size={20} />
